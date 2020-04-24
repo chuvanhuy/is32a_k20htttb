c2f2531993fa8d12f9a5a12bb44838080d2cbd23
Chu Van Huy <chuvanhuy@gmail.com>
2020-04-24 11:32:56 +0700

    Mã nguồn ngày 24/04/2020

--- trang_chu.php
+++ trang_chu.php
@@ -13,11 +13,125 @@
 </head>
 <body>
 	<?php
+		/* 
+		- Tác giả: Đoàn Thanh Huyền
+		- Ngày sửa đổi gần nhất: 24/04/2019
+		- Phiên bản: v1.1
+		*/
+
+		// Đây là câu lệnh in dữ liệu ra màn hình
 		echo "XIN CHÀO! ĐÂY LÀ DÒNG MÃ LỆNH PHP ĐẦU TIÊN CỦA TÔI";
 
+		// Đây là câu lệnh in dữ liệu ra màn hình
+			# Hàm echo để in
+			# Hàm date() để xuất dữ liệu ngày tháng
+			# Tham số d: chỉ ngày
+			# Tham số m: chỉ tháng
+			# Tham số Y: chỉ năm
 		echo "<br>"."Hôm là ngày: ".date("d/m/Y");
 
-		echo "<br>"."Bây giờ là: ".date("h:i:sa");
+		// Đây là câu lệnh in dữ liệu ra màn hình câu lệnh print_r
+			# Hàm print_r để in
+			# Hàm date() để xuất dữ liệu ngày tháng
+			# Tham số h: chỉ giờ
+			# Tham số i: chỉ phút
+			# Tham số s: chỉ giây
+			# Tham số a: chỉ AM hoặc PM
+		print_r("<br>"."Bây giờ là: ".date("h:i:s a"));
+
+		// Khởi tạo một biến
+		$sinhvien = "Trần Kim Dung";
+
+		// Kiểm tra thông tin chi tiết của biến thông qua hàm var_dump()
+		echo "<br><br>";
+		var_dump($sinhvien);
+
+		// Khởi tạo 1 biến chưa định nghĩa (hay nó ở trạng thái NULL)
+		$giangvien;
+
+		// Kiểm tra thông tin chi tiết của biến thông qua hàm var_dump()
+		echo "<br><br>";
+		var_dump($giangvien);
+
+		// Dùng hàm isset() để check xem 1 biến nào đó đã được khởi tạo và gán giá trị hay chưa?
+		echo "<br><br>";
+		$giangvien = "Chu Văn Huy";
+		if(isset($giangvien)) {
+			echo "Biến giangvien đã được gán giá trị";
+		} else {
+			echo "Biến giangvien chưa được gán giá trị";
+		}
+
+		// Dùng hàm empty() để check xem 1 biến nào đó có rỗng hay không?
+		echo "<br><br>";
+		if(empty($giangvien)) {
+			echo "Biến giangvien rỗng";
+		} else {
+			echo "Biến giangvien không rỗng";
+		}
+
+		// Thử so sánh bằng
+		echo "<br><br>";
+		if ($giangvien != "Đoàn Thanh Huyền") {
+			echo "Oh, cô Huyền không dạy lớp mình";
+		} else {
+			echo "Vui quá. Cô Huyền dạy lớp mình!";
+		}
+
+		// Cách 1: viết ĐẨY ĐỦ câu lệnh IF ELSE
+		echo "<br><br>";
+		if ($giangvien != "Đoàn Thanh Huyền") {
+			$ketqua="Oh, cô Huyền không dạy lớp mình";
+		} else {
+			$ketqua="Vui quá. Cô Huyền dạy lớp mình!";
+		}
+		echo "Cách 1: ".$ketqua;
+
+
+		// Cách 2: viết RÚT GỌN câu lệnh IF ELSE
+		echo "<br><br>";
+		$ketqua = ($giangvien != "Đoàn Thanh Huyền") ? "Oh, cô Huyền không dạy lớp mình" : "Vui quá. Cô Huyền dạy lớp mình!";
+		echo "Cách 2: ".$ketqua;
+
+		// Tìm hiểu cách thực hiện vòng lặp FOR
+		echo "<br><br>";
+		for ($i=1; $i <= 10 ; $i++) { 
+			echo "Lần thông báo bằng vòng FOR thứ ".$i."<br>";
+		}
+
+		// Tìm hiểu cách thực hiện vòng lặp WHILE
+		echo "<br><br>";
+		$i = 1;
+		while ($i <= 10) {
+			echo "Lần thông báo bằng vòng WHILE thứ ".$i."<br>";
+			$i++;
+		}
+
+		// Khai báo 1 mảng như thế nào?
+		$nguoiyeu = array("Mơ", "Mận", "Đào", "Lê", "Huyền");
+
+		// In ra được phần tử đầu tiên (cô thứ nhất)
+		echo "<br><br>";
+		echo "Cô đầu tiên: ".$nguoiyeu[0];
+
+		// In ra được phần tử cuối cùng (cô hiện giờ)
+		echo "<br><br>";
+		echo "Cô hiện giờ: ".$nguoiyeu[4];
+
+		// Khai báo 1 mảng
+		$xehoi = array(
+			"ten" => "Kia Morning",
+			"nam_san_xuat" => 2014,
+			"gia_tien" => "350.000.000 đ"
+		);
+
+		// In ra được các thành phần của mảng
+		echo "<br><br>";
+		echo $xehoi["ten"];
+
+
+		// Hàm exit() có tác dụng THOÁT, không thực thi các câu lệnh phía dưới
+		exit();
 	;?>
 
 	<h1 style="color: red;">Chào mừng các bạn ghé thăm Website K20HTTTB</h1>
